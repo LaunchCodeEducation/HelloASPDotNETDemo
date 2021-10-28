5576f90b1e1d13f32d924533fd817212a515ab86
adamstgr <adam.stgr@gmail.com>
2021-10-28 13:54:50 -0400

    created the project and added items through 13.6

---
+++ CodingEvents/CodingEvents/CodingEvents/Controllers/EventsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodingEvents.Controllers
+{
+    public class EventsController : Controller
+    {
+        static private List<string> Events = new List<string>();
+        [HttpGet]
+        public IActionResult Index()
+        {
+            Events.Add("Code the Kasbah");
+            Events.Add("Bringing Coding Back");
+            Events.Add("Bohemian Codesody");
+            ViewBag.events = Events;
+            return View();
+        }
+    }
+}
